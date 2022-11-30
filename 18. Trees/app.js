742d2ad80ed4b102e1a0334865885c673430b6ab
TAACHRA0 <rahul.choudhary@swisscom.com>
2022-11-30 15:37:45 +0100

    update

---
+++ 18. Trees/app.js
@@ -0,0 +1,10 @@
+//////////////////////
+// TREE DATA STRUCTURE
+//////////////////////
+
+// CONSIST OF NODES IN A PARENT/CHILD RELATIONSHIP
+
+// ROOT, CHILD, PARENT, SIBLING (Node with same parent),
+// LEAF (Node with no children), EDGE (Connection between node)
+
+// USES: HTML DOM, NETWORK ROUTING, FOLDER STRUCTURE IN COMPUTER
